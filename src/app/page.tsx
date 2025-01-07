a690ae38162a8e28dcc4951294617d8b01d0478b
Francesco Burato <francesco.burato@gmail.com>
2025-01-07 11:57:08 +0100

    Create management for local files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useState } from "react"
+
+type JSONValue = 
+ | string
+ | number
+ | boolean
+ | null
+ | JSONValue[]
+ | {[key: string]: JSONValue}
+
+// interface JSONObject {
+//   [k: string]: JSONValue
+// }
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [fileContent, setFileContent] = useState< JSONValue | null>(null)
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  return (
+    <main>
+      <div>
+        <input type="file" name="file" onChange={async (event) => {
+          const fileList = event.target.files
+          if (fileList) {
+            const [file] = fileList
+            const fileContent = await file.text()
+            setFileContent(JSON.parse(fileContent) as JSONValue)
+          }
+        }}/>
+        { !fileContent ? <></> : <a href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(fileContent))}`} download={"state.json"}><button>Download content</button></a>}
+        <div>
+          { !fileContent ? (<p>File not read yet</p>) : (<div><p>File content:</p><p>{JSON.stringify(fileContent)}</p></div>) }
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
