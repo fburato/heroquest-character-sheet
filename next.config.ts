0a75350fa0fb6e7fae9e3e9274d73449bc7cf71b
Francesco Burato <francesco.burato@gmail.com>
2025-01-08 23:22:26 +0100

    Correct basePath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: "/heroquest-character-sheet"
 };
 
 export default nextConfig;
