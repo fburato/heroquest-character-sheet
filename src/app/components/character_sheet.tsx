7fda478ed04271893512770710d1530df9e5674d
Francesco Burato <francesco.burato@gmail.com>
2025-01-08 11:19:05 +0100

    Complete CSS structure

--- src/app/components/character_sheet.tsx
+++ src/app/components/character_sheet.tsx
@@ -9,40 +9,42 @@ const NumericFieldWithBase: FC<{
     label: string,
 }> = ({ valueId, label }) => {
     return (
-        <div className="numericFieldWithBase numericField" id={valueId}>
-            <span className="numericFieldLabel">{label}</span>
+        <>
+            <div className="numericFieldLabel" id={valueId + "Label"}>
+                <span>{label}</span>
+            </div>
+            <button className="minus">-</button>
+            <div className="fields">
+                <input type="number" id={valueId + "Value"} name={valueId + "Value"} className="numericFieldInput" />
+                <input type="number" id={valueId + "Base"} name={valueId + "Base"} className="baseNumericFieldInput" />
+            </div>
+            <button className="plus">+</button>
+        </>
+    )
+}
+
+const NumericField: FC<{ valueId: string, label: string }> = ({ valueId, label }) => {
+    return (
+        <>
+            <div className="numericFieldLabel" id={valueId + "Label"}>
+                <span>{label}</span>
+            </div>
             <div>
-                <button className="minus">-</button>
                 <div className="inputs">
                     <input type="number" id={valueId + "Value"} name={valueId + "Value"} className="numericFieldInput" />
-                    <input type="number" id={valueId + "Base"} name={valueId + "Base"} className="baseNumericFieldInput" />
                 </div>
-                <button className="plus">+</button>
             </div>
-        </div>
+        </>
     )
 }
 
-const NumericField: FC<{valueId: string, label: string}> = ({valueId, label}) => {
+const Quantity: FC<{ quantity: number }> = ({ quantity }) => {
     return (
-        <div className="numericField" id={valueId}>
-            <span className="numericFieldLabel">{label}</span>
-            <div>
-            <div className="inputs">
-                <input type="number" id={valueId + "Value"} name={valueId + "Value"} className="numericFieldInput"/>
-            </div>
-            </div>
-        </div>
-    )
-}
-
-const Quantity: FC<{quantity: number, id: number}> = ({quantity, id}) => {
-    return (
-        <div className="quantity" id={"qty-" + id}>
+        <>
             <button className="minus" aria-label="Decrease">&minus;</button>
             <input type="number" className="input-box" value={quantity} />
             <button className="plus" aria-label="Increase">+</button>
-        </div>
+        </>
     )
 }
 
@@ -52,33 +54,30 @@ enum ButtonPosition {
     Last,
 }
 
-const ActionButton: FC<{position: ButtonPosition}> = ({position}) => {
+const ActionButton: FC<{ position: ButtonPosition }> = ({ position }) => {
     return (
-        <div className="actionButton">
-            <div>
-                <button className="up" aria-label="Move up" style={{visibility: (position == ButtonPosition.First ? "hidden" : "visible")}}>&#x2191;</button>
-                <button className="delete" aria-label="Delete">&#x2573;</button>
-                <button className="down" aria-label="Move down" style={{visibility: (position == ButtonPosition.Last ? "hidden" : "visible") }}>&#x2193;</button>
-            </div>
-        </div>
+        <>
+            <button className="up" aria-label="Move up" style={{ visibility: (position == ButtonPosition.First ? "hidden" : "visible") }}>&#x2191;</button>
+            <button className="delete" aria-label="Delete">&#x2573;</button>
+            <button className="down" aria-label="Move down" style={{ visibility: (position == ButtonPosition.Last ? "hidden" : "visible") }}>&#x2193;</button>
+        </>
     )
 }
 
 //eslint-disable-next-line  @typescript-eslint/no-empty-object-type
-const Inventory: FC<{}> = ({}) => {
+const Inventory: FC<{}> = ({ }) => {
     return (
-    <div id="inventory">
-        <ul id="inventoryList">
-            <li><Quantity quantity={9} id={0} /><div className="item">Item 123 123 123 123 123 123 123 123 123</div><ActionButton position={ButtonPosition.First}/></li>
-            <li><Quantity quantity={10} id={1} /><div className="item">Item 1</div><ActionButton position={ButtonPosition.Middle}/></li>
-            <li><Quantity quantity={11} id={2} /><div className="item">Item 2</div><ActionButton position={ButtonPosition.Last}/></li>
-        </ul>
-        <div id="newItemForm">
-            <label htmlFor="newItem">New Item:</label>
-            <input type="text" id="newItem" name="newItem" />
-            <button aria-label="Add item">Add</button>
-        </div>
-    </div>);
+        <div id="inventory">
+            <Quantity quantity={9} id={0} />
+            <div className="item">Item 123 123 123 123 123 123 123 123 123</div>
+            <ActionButton position={ButtonPosition.First} />
+            <Quantity quantity={10} id={1} />
+            <div className="item">Item 1</div>
+            <ActionButton position={ButtonPosition.Middle} />
+            <Quantity quantity={11} id={2} />
+            <div className="item">Item 2</div>
+            <ActionButton position={ButtonPosition.Last} />
+        </div>);
 }
 
 export default function CharacterSheet() {
@@ -88,25 +87,33 @@ export default function CharacterSheet() {
                 <h1 id="header">Character sheet</h1>
             </section>
             <section id="general">
-                    <label htmlFor="characterName">Name:</label>
-                    <input type="text" id="characterName" name="characterName" />
-                
-                    <label htmlFor="characterType">Character:</label>
-                    <input type="text" id="characterType" name="characterName" />
+                <label htmlFor="characterName">Name:</label>
+                <input type="text" id="characterName" name="characterName" />
+                <label htmlFor="characterType">Character:</label>
+                <input type="text" id="characterType" name="characterName" />
             </section>
             <hr />
             <section id="characterValues">
-                <NumericFieldWithBase valueId="attackDice" label="Attack dice:" />
-                <NumericFieldWithBase valueId="defendDice" label="Defend dice:" />
-                <NumericFieldWithBase valueId="bodyPoints" label="Body points:" />
-                <NumericFieldWithBase valueId="mindPoints" label="Mind points:" />
-                <NumericField valueId="goldCoins" label="Gold coins:" />
-                <NumericField valueId="quests" label="Quests:" />
+                <div id="characterValuesBase">
+                    <NumericFieldWithBase valueId="attackDice" label="Attack dice:" />
+                    <NumericFieldWithBase valueId="defendDice" label="Defend dice:" />
+                    <NumericFieldWithBase valueId="bodyPoints" label="Body points:" />
+                    <NumericFieldWithBase valueId="mindPoints" label="Mind points:" />
+                </div>
+                <div id="characterValuesFree">
+                    <NumericField valueId="goldCoins" label="Gold coins:" />
+                    <NumericField valueId="quests" label="Quests:" />
+                </div>
             </section>
             <hr />
-            <section id="inventory">
+            <section id="inventorySection">
                 <h1>Inventory</h1>
                 <Inventory />
+                <div id="newItemForm">
+                    <label htmlFor="newItem">New Item:</label>
+                    <input type="text" id="newItem" name="newItem" />
+                    <button aria-label="Add item">Add</button>
+                </div>
             </section>
             <hr />
             <section id="notes">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,25 @@
 import { useState } from "react"
 import CharacterSheet from "./components/character_sheet"
 
-type JSONValue = 
- | string
- | number
- | boolean
- | null
- | JSONValue[]
- | {[key: string]: JSONValue}
+type JSONValue =
+  | string
+  | number
+  | boolean
+  | null
+  | JSONValue[]
+  | { [key: string]: JSONValue }
 
 // interface JSONObject {
 //   [k: string]: JSONValue
 // }
 
 export default function Home() {
-  const [fileContent, setFileContent] = useState< JSONValue | null>(null)
+  const [fileContent, setFileContent] = useState<JSONValue | null>(null)
 
   return (
     <main>
       <div>
         <CharacterSheet />
-        <div style={{width: "200px"}}>
-          <img src="body_points.svg" alt="Body Points"/>
-        </div>
-        
         <input type="file" name="file" onChange={async (event) => {
           const fileList = event.target.files
           if (fileList) {
@@ -32,10 +28,10 @@ export default function Home() {
             const fileContent = await file.text()
             setFileContent(JSON.parse(fileContent) as JSONValue)
           }
-        }}/>
-        { !fileContent ? <></> : <a href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(fileContent))}`} download={"state.json"}><button>Download content</button></a>}
+        }} />
+        {!fileContent ? <></> : <a href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(fileContent))}`} download={"state.json"}><button>Download content</button></a>}
         <div>
-          { !fileContent ? (<p>File not read yet</p>) : (<div><p>File content:</p><p>{JSON.stringify(fileContent)}</p></div>) }
+          {!fileContent ? (<p>File not read yet</p>) : (<div><p>File content:</p><p>{JSON.stringify(fileContent)}</p></div>)}
         </div>
       </div>
     </main>
